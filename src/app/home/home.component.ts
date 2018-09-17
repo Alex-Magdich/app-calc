36d88764d6857921a6924144549b3d1591b1f0d2
Alex-Magdich <alexkotorij@gmail.com>
2018-09-17 17:33:18 +0300

    todo list

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number ;
+  btnText: string = 'Добавить задачу';
+  goalText: string = 'Первая задача';
+  goals = [];
+  constructor() { }
+
+  ngOnInit() {
+      this.itemCount = this.goals.length;
+  }
+  addItem(){
+      this.goals.push(this.goalText);
+      this.goalText = '';
+      this.itemCount = this.goals.length;
+  }
+
+}
